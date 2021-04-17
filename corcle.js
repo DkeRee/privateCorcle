5edcc52b21444f133b99f8d39ba28ce68d597deb
DkeRee <dke111us@gmail.com>
2021-04-17 11:36:20 -0700

    quick fix on DOM scrolling down

--- corcle.js
+++ corcle.js
@@ -26,7 +26,13 @@ titleScreen.volume = 0.15;
 
 $(body).bind('contextmenu', function(e) {
   return false;
-}); 
+});
+
+window.addEventListener("keydown", e => {
+  if (e.keyCode == 32 && e.target == document.body){
+    e.preventDefault();
+  }
+});
 
 var engine = Matter.Engine.create({
   bounds: {
